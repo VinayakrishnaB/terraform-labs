d2320fd0340c97a2fb8d277d95a6f9a26e9e4bdb
VinayakrishnaB <krishna.vinaya@gmail.com>
2022-05-10 13:48:46 +0530

    updated file

--- first_ec2.tf
+++ first_ec2.tf
@@ -7,30 +7,6 @@ terraform{
   }
 }
 
-provider "aws" {
-region = "us-west-2"
-access_key = "AKIAT4FDAEWY2OAZ2JOX"
-secret_key = "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"
-}
-/*
-resource "aws_key_pair" "loginkey" {
-  key_name = "mynew_keypair1"
-  public_key = file("C:\\Users\\Vinzi\\.ssh\\id_rsa.pub")
-
-}
-
-resource aws_security_group "linuxgrp"{
-  name = "mynewSG"
-
-  ingress{
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
-*/
 
 resource "aws_instance" "myec2" {
   ami = "ami-0ca285d4c2cda3300"
